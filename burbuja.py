c07bba0ca36a78c2f617ae4e85358fea287ee6b0
CarpioVargas <rbtcarpio@gmail.com>
2016-09-18 14:05:51 -0700

    no message

--- burbuja.py
+++ burbuja.py
@@ -9,19 +9,15 @@ def ordenamientoBurbuja(lista):
 	""" Ordenar una lista utilizando el metodo de Burbuja """
 
 	for i in range (1, len(lista)):
-
 		# Scanea la lista, quitando los valores ya ordenados
 		for j in range (len(lista) - i):
-
 			# La posicion actual es mayor a la siguiente?
 			if (lista[j] > lista[j+1]):
-
 				# Intercambia los valores de la posicion actual con la siguiente 
 				lista[j+1], lista[j] = lista[j], lista[j+1]
 	print lista
 
 ordenamientoBurbuja(lista)
-
 #Fin del tiempo de ejecucion
 tiempo_final = time()
 print tiempo_final - tiempo_inicial
