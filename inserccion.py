d3a87a2df690d744125c12dd818e14a9d9f17c58
CarpioVargas <rbtcarpio@gmail.com>
2016-09-18 13:54:02 -0700

    no message

---
+++ inserccion.py
@@ -0,0 +1,31 @@
+from time import time
+
+lista = [2, 8, 6, 1, 9, 0, -4]
+
+#inicia el tiempo de ejecucion
+tiempo_inicial = time()
+
+def ordenamientoInserccion(lista):
+    
+    for i in range(len(lista)):
+    	#Encontrar la posicion con el numero mas chica
+    	#Empezar desde la posicion actual
+        pos_min = i
+
+        for j in range(i + 1, len(lista)):
+
+        	#Es esta posicion mas chica?
+            if lista[j] < lista[pos_min]:
+
+            	#Si lo es marcar como la mas chica
+                pos_min = j
+
+            #Intercambiar los dos valores    
+        	lista[pos_min], lista[i] = lista[i], lista[pos_min]
+        
+    print lista
+
+ordenamientoInserccion(lista)
+#termina el tiempo de ejecucion
+tiempo_final = time()
+print tiempo_final - tiempo_inicial
