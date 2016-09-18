6c3c1a2453cc7cbcfb7d17e0f839d99b0b129600
CarpioVargas <rbtcarpio@gmail.com>
2016-09-18 12:34:21 -0700

    no message

--- burbuja.py
+++ burbuja.py
@@ -4,11 +4,10 @@ lista = [2, 19, 10, 1, 40]
 tiempo_inicial = time()
 
 def ordenamientoBurbuja(lista):
-	for i in range (1, 	len(lista)):
+	for i in range (1, len(lista)):
 		for j in range (len(lista) - i):
 			if(lista[j] > lista[j+1]):
 				lista[j+1], lista[j] = lista[j], lista[j+1]
-
 	print lista
 
 ordenamientoBurbuja(lista)
