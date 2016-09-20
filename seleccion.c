d73c44f64cfa7c34332bc44667ca86bbc421905a
josmanuelsolano <josmanuelsolano@gmail.com>
2016-09-19 21:30:13 -0700

    se me olvido calcular el tiempo :P

--- seleccion.c
+++ seleccion.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 //ordenamiento por seleccion
 
 void seleccion(int lista[]){
   int i, j, tmp, pos_min;
   int n = 10;
+  clock_t begin = clock();
   for (i = 0; i < n-1; i++) {
     pos_min = i;
     for (j = i+1; j < n; j++) {
@@ -16,9 +18,13 @@ void seleccion(int lista[]){
     lista[i] =  lista[pos_min];
     lista[pos_min] = tmp;
   }
+  clock_t end = clock();
+  float time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   for (i = 0; i < n; i++) {
     printf("%d -- ",lista[i]);
   }
+  printf("\n");
+  printf("tiempo consumido en ejecucion: %f",time_spent );
   printf("\n");
 }
 
