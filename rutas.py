dad7c344c767f21146d0de0e54ffa7f0747e294f
CarpioVargas <rbtcarpio@gmail.com>
2016-09-28 23:57:56 -0700

    cambios

--- rutas.py
+++ rutas.py
@@ -2,42 +2,34 @@
 # [2] = [1, 3, 4, 5, 8]
 # [3] = [1, 2, 4]
 # [4] = [2, 3, 5, 8]
-# [5] = [2, 4]
+# [5] = [2, 4, 6]
 # [6] = [1, 7]
 # [7] = [6, 8]
 # [8] = [1, 2, 4, 7]
 
-lst	 = [	
-			[2,3,6,8,0], 
-			[1,3,4,5,8],
-			[1,2,4,0,0],
-			[2,3,5,8,0],
-			[2,4,0,0,0],
-			[1,7,0,0,0],
-			[6,8,0,0,0],
-			[1,2,4,7,0]
-		]
+mat = {'1': ['2', '3', '6', '8'],
+         '2': ['1', '3','4','5','8'],
+         '3': ['1','2','4'],
+         '4': ['2','3','5','8'],
+         '5': ['2','4','6'],
+         '6': ['1','7'],
+         '7': ['6','8'],
+         '8': ['1','2', '4', '7']
+         }
 
-def findthis(lst, start, end, current):
-	start -= 1
+def findthis(mat, start, end, route=[]):
+	route = route + [start]
+
 	if start == end:
-		return
+		return route
 
-	currentArray = lst[start]
-	for i in range (len(currentArray)):
-		if lst[start][i] == end:
-			print start +1, lst[start]
-			return
-	
-	if current < len(currentArray):
-		start = currentArray[current]
-		current += 1
-		findthis(lst, start, end, current)
-	else:
-		findthis(lst, currentArray[0], end, current)
-	
+	for i in mat[start]:
+		print i
+		if i not in route:
+			newroute = findthis(mat, i, end, route)
+			return newroute
+	return None
 
-
-findthis(lst, 1, 5, 0)
-	
-
+start = raw_input("Dame el inicio: ")
+end = raw_input("Dame el destino: ")
+print findthis(mat, start, end)
